d23726bfca95019360402976d6e8d74a01bf2a5c
Julian Sehbaoui <yassinjulian@gmail.com>
2021-05-18 13:18:08 +0200

    You may enter test mode, does not work yet

--- components/board_cls.py
+++ components/board_cls.py
@@ -5,14 +5,17 @@ class Board:
     game_draws = []
     game_over = False
     resign = False
+    test_mode = False
 
-    def __init__(self, master, width, height, tile_size, color_a, color_b, anchor_point, border_color = (152, 186, 0)):
+    def __init__(self, master, width, height, tile_size, color_a, color_b, color_t1, color_t2, anchor_point, border_color = (152, 186, 0)):
         self.master = master
         self.width = width
         self.height = height
         self.tile_size = tile_size
         self.color_a = color_a
         self.color_b = color_b
+        self.color_t1 = color_t1
+        self.color_t2 = color_t2
         self.borderColor = border_color
         self.anchor_point = anchor_point
 
@@ -31,37 +34,42 @@ class Board:
         tile_dir_list = []
         font = pygame.font.Font(None, 20)
 
+        if Board.test_mode:
+            color = (self.color_t1, self.color_t2)
+        else:
+            color = (self.color_a, self.color_b)
+
         for y in range(8):
             tile_dir_list.append([])
             for x in range(8):
                 tile_dir_list[y].append((x*self.tile_size, y*self.tile_size))
                 if y % 2 == 0:
                     if x % 2 == 0:
-                        pygame.draw.rect(self.master, (245, 216, 188), [x*self.tile_size, y*self.tile_size, self.tile_size, self.tile_size])
+                        pygame.draw.rect(self.master, color[0], [x*self.tile_size, y*self.tile_size, self.tile_size, self.tile_size])
                     else:
-                        pygame.draw.rect(self.master, (176, 142, 109), [x*self.tile_size, y*self.tile_size, self.tile_size, self.tile_size])
+                        pygame.draw.rect(self.master, color[1], [x*self.tile_size, y*self.tile_size, self.tile_size, self.tile_size])
                 else:
                     if x % 2 == 0:
-                        pygame.draw.rect(self.master, (176, 142, 109), [x*self.tile_size, y*self.tile_size, self.tile_size, self.tile_size])
+                        pygame.draw.rect(self.master, color[1], [x*self.tile_size, y*self.tile_size, self.tile_size, self.tile_size])
                     else:
-                        pygame.draw.rect(self.master, (245, 216, 188), [x*self.tile_size, y*self.tile_size, self.tile_size, self.tile_size])
+                        pygame.draw.rect(self.master, color[0], [x*self.tile_size, y*self.tile_size, self.tile_size, self.tile_size])
 
         for x in range(8):
             if x%2 == 0:
-                color = (245, 216, 188)
+                color_l = color[0]
             else:
-                color = (176, 142, 109)
+                color_l = color[1]
             
-            label = font.render(chr(97+x), True, color)
+            label = font.render(chr(97+x), True, color_l)
             self.master.blit(label, (x*self.tile_size+5, 8*self.tile_size-15))
 
         for y in range(8):
             if y%2 == 0:
-                color = (245, 216, 188)
+                color_l = color[0]
             else:
-                color = (176, 142, 109)
+                color_l = color[1]
             
-            label = font.render(str(8-y), True, color)
+            label = font.render(str(8-y), True, color_l)
             self.master.blit(label, (8*self.tile_size-15, y*self.tile_size+5))
 
 
@@ -125,6 +133,15 @@ class Board:
         screen_.blit(label, (s.get_width()/2 - label.get_width()/2,s.get_height()/2-label.get_height()/2))
         screen_.blit(s, (0,0))    # (0,0) are the top-left coordinates
         Board.game_over = True
+
+    @classmethod
+    def change_testmode(cls):
+        cls.test_mode = not cls.test_mode
+
+    def build_from_list():
+        pass
+
+
 
 
         
